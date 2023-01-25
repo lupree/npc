3e6bde2557f793d489a5e51e8403640545c5ab61
Luca Ramseyer <luca.ramseyer@lupree.com>
2023-01-25 23:11:56 +0100

    initial commit new bot

---
+++ src/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config()
+const fs = require("fs")
+const { Client, Collection, InteractionType } = require("discord.js")
+
+const client = new Client({intents:[]})
+client.commands = new Collection()
+
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js')); 
+
+commandFiles.forEach((commandFile) => {
+    const command = require(`./commands/${commandFile}`)
+    client.commands.set(command.data.name, command)
+})
+
+client.once("ready", () => {
+    
+    console.log(`Ready! Logged in as ${client.user.tag}! I'm on ${client.guilds.cache.size} guild(s)!`)
+    client.user.setActivity({name: "mit dem Code", type: "PLAYING"})
+    
+})
+
+client.on("interactionCreate", async (interaction) => {
+    if(interaction.type !== InteractionType.ApplicationCommand) return;
+
+    if(interaction.commandName === 'ping'){
+        await interaction.reply('lul');
+    }
+})
+
+client.login(process.env.TOKEN)
